d4d35b54279e401dd4df437c73256d5f8de5c324
Shruti Shrivastava <shrutishri2208@gmail.com>
2023-08-12 03:21:05 +0530

    github-button-animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -20,6 +20,8 @@ const Card = ({
 }) => {
   const theme = useSelector((state) => state.theme.theme);
 
+  const [githubHover, setGithubHover] = useState(false);
+
   return (
     <div
       className={`card ${theme === "light" ? "card-light" : "card-dark"} flex ${
@@ -89,9 +91,37 @@ const Card = ({
         <h2 className="mb-4">{desc}</h2>
         <div className="flex justify-between items-center mt-8">
           <p>{tech}</p>
-          <div className="github-button flex gap-2 items-center justify-center">
-            <img src={github} className="h-6 w-6" />
-            <span className="">GitHub</span>
+
+          <div
+            className="github-button relative overflow-hidden"
+            onMouseEnter={() => setGithubHover(true)}
+            onMouseLeave={() => setGithubHover(false)}
+            onMouseOver={() => setGithubHover(true)}
+          >
+            <div
+              className={`flex gap-3 items-center justify-center relative left-7 ${
+                githubHover === true
+                  ? "github-button-move"
+                  : "github-button-back"
+              }`}
+            >
+              <img src={github} className="h-6 w-6 absolute -left-8" />
+              <span className="">GitHub</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="9"
+                height="14"
+                viewBox="0 0 9 14"
+                fill="none"
+                style={{ position: "relative", top: "1px", left: "5px" }}
+              >
+                <path
+                  d="M1 1C3.20914 3.20914 4.44771 4.44771 6.65685 6.65685L1 12.3137"
+                  stroke="black"
+                  stroke-width="2"
+                />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
