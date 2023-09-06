a729ff3699b04116a4908c88a9ac22ba2005d5aa
Shruti Shrivastava <shrutishri2208@gmail.com>
2023-09-06 23:34:22 +0530

    footer-issue-2

--- src/components/FooterItem.js
+++ src/components/FooterItem.js
@@ -18,10 +18,30 @@ const FooterItem = ({ title, color, link }) => {
       >
         <p className="py-10 text-2xl font-semibold ">{title}</p>
         <div
-          className="footer-bg w-full flex items-center 2xl:px-32 xl:px-32 px-8"
+          className="footer-bg w-full flex 2xl:px-32 xl:px-32 px-8 flex justify-between items-center"
           style={{ backgroundColor: `${color}` }}
         >
           <p className="text-2xl font-semibold ">{title}</p>
+          <a href={link} target="_blank">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="60"
+              height="60"
+              viewBox="0 0 40 40"
+              fill="none"
+            >
+              <path
+                d="M14 15C18.6863 15 21.3137 15 26 15V27"
+                stroke={`${theme === "light" ? "black" : "white"}`}
+                strokeWidth={2}
+              />
+              <path
+                d="M26 15L14 27"
+                stroke={`${theme === "light" ? "black" : "white"}`}
+                strokeWidth={2}
+              />
+            </svg>
+          </a>
 
           {/* <div className="flex gap-8 flex-nowrap h-full moving-footer ">
             {myArray.map((item, index) => {
