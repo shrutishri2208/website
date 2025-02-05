7a14a96c75418dccce8ee44cbae1aef4595a8b4f
Shruti Shrivastava <shrutishri2208@gmail.com>
2025-02-05 18:16:40 +0530

    smotth loading of videos

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const VideoPlayer = ({ src }) => {
+  const [showVideo, setShowVideo] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowVideo(true);
+    }, 200);
+  }, []);
+
   return (
-    <div className="h-full justify-center items-center">
+    <div className="h-screen justify-center items-center">
+      <div className="h-[560px] w-64 bg-black rounded-lg absolute" />
+
       <video
-        className="h-[560px] w-auto rounded-lg"
+        className={`h-[560px] w-auto rounded-lg relative z-10 transition-opacity duration-500 ${
+          showVideo ? "opacity-100" : "opacity-0"
+        }`}
         autoPlay
         loop
         muted
