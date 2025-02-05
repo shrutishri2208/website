950e896cdf1a3c87e835b3b2a44ca3f1ee8afdff
Shruti Shrivastava <shrutishri2208@gmail.com>
2025-02-05 15:40:15 +0530

    added all 4 pages

--- src/pages/DMs.js
+++ src/pages/DMs.js
@@ -1,7 +1,26 @@
 import React from "react";
+import BackArrow from "../components/BackArrow";
+import VideoPlayer from "../components/VideoPlayer";
+import DmsVideo from "../assets/Dms.mp4";
 
 const DMs = () => {
-  return <div></div>;
+  return (
+    <div
+      className={
+        "bg-black h-full w-full flex flex-col min-h-screen pt-10 px-14"
+      }
+    >
+      <BackArrow />
+      <div className={"pt-7 px-14 pb-4"}>
+        <h1 className="text-white text-5xl font-bold mb-16">
+          Direct Messaging
+        </h1>
+        <div className="justify-center gap-28  flex">
+          <VideoPlayer src={DmsVideo} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DMs;
