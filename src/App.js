cafe359e8c9dc633c20629938dbad00b792fdee1
Shruti Shrivastava <shrutishri2208@gmail.com>
2025-02-06 21:25:20 +0530

    analytics

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import News from "./pages/News";
 import Salaries from "./pages/Salaries";
 import DMs from "./pages/DMs";
 import Onboarding from "./pages/Onboarding";
+import ReactGA from "react-ga4";
+import AnalyticsTracker from "./services/AnalyticsTracker";
 
 function App() {
+  ReactGA.initialize("G-4B3EP79PND");
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
     <Router>
+      <AnalyticsTracker />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/news" element={<News />} />
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const VideoPlayer = ({ src }) => {
   const [showVideo, setShowVideo] = useState(false);
+  const videoRef = useRef(null);
 
   useEffect(() => {
     setTimeout(() => {
@@ -9,11 +10,28 @@ const VideoPlayer = ({ src }) => {
     }, 200);
   }, []);
 
+  const handleMouseEnter = () => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+    }
+  };
+
   return (
-    <div className="h-screen justify-center items-center">
+    <div
+      className="h-screen justify-center items-center"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="h-[560px] w-64 bg-black rounded-lg absolute" />
 
       <video
+        ref={videoRef}
         className={`h-[560px] w-auto rounded-lg relative z-10 transition-opacity duration-500 ${
           showVideo ? "opacity-100" : "opacity-0"
         }`}
